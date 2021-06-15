83cd7f295bee3c43bd5c4389b089af385864ce61
Keigh Rim <keigh.rim@gmail.com>
2021-06-15 14:49:52 -0400

    deleted unused files

--- outerdemo.sh
+++
@@ -1,5 +0,0 @@
-rm -r ./demo/results-old && mv ./demo/results ./demo/results-old || echo "No old results, continuing"
-docker run --gpus all --entrypoint /bin/bash app-ina-segmenter:latest demo.sh
-docker cp "$(docker ps -lq)":/segmenter/demo/ ./demo/results/
-docker rm "$(docker ps -lq)"
-echo "Done. Check ./demo/results for the results of the demo."
